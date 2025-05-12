5724ae7aa4d6ec6fa411ac5f0873b7f477556e66
54M07 <tomas_ss@aol.com>
2025-05-12 17:43:20 -0300

    atualizando

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  const observer = new IntersectionObserver(
+      (entries) => {
+          entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                  entry.target.classList.add("visible");
+              } else {
+                  entry.target.classList.remove("visible");
+              }
+          });
+      },
+      {
+          root: null, 
+          threshold: 0.5,
+      }
+  );
+
+  cards.forEach((card) => observer.observe(card));
+});
+
 let tablinks = document.getElementsByClassName("tab__links");
 let tabcontents = document.getElementsByClassName("tab__contents");
 
